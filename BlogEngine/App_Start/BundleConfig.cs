456459936953a92412555fc30edc805e7551dea3
Tim <tim@timcollins.ie>
2014-09-13 21:31:55 +0100

    Added blog template CSS and example HTML to default view. Removed default site.css as it's not used.

--- BlogEngine/App_Start/BundleConfig.cs
+++ BlogEngine/App_Start/BundleConfig.cs
@@ -7,10 +7,13 @@ namespace BlogEngine
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/blog.css")
+                .Include("~/Content/bootstrap.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-1.9.1.js")
+                .Include("~/Scripts/bootstrap.js"));
         }
     }
 }
