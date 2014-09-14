4c455b8feab716f51a8c2a7415328678bf2bae49
Tim <tim@timcollins.ie>
2014-09-14 20:44:39 +0100

    Added CommentRepository.GetComments()

--- BlogEngine/Models/Entities/Comment.cs
+++ BlogEngine/Models/Entities/Comment.cs
@@ -2,9 +2,9 @@
 {
     public class Comment : BaseEntity
     {
-        public int ID { get; set; }
+        public long ID { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
-        public int PostID { get; set; }
+        public long PostID { get; set; }
     }
 }
--- BlogEngine/Models/Repositories/CommentRepository.cs
+++ BlogEngine/Models/Repositories/CommentRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using BlogEngine.Models.DataAccess;
 using BlogEngine.Models.Entities;
 
 namespace BlogEngine.Models.Repositories
@@ -7,7 +10,41 @@ namespace BlogEngine.Models.Repositories
     {
         public List<Comment> GetComments(long postID)
         {
-            return new List<Comment>();   
+            const string sqlQuery = "SELECT ID, Title, Body, CreatedOn, CreatedBy, ModifiedOn, ModifiedBy " +
+                                    "FROM Comment " +
+                                    "WHERE PostID = @postID " + 
+                                    "ORDER BY CreatedOn DESC ";
+
+            List<Comment> comments = new List<Comment>();
+
+            using (var reader = DbUtil.ExecuteReader(sqlQuery, new SQLiteParameter("@postID", postID)))
+            {
+                while (reader.Read())
+                {
+                    comments.Add(ReadComment(reader));
+                }
+            }
+
+            foreach (Comment comment in comments)
+            {
+                comment.PostID = postID;
+            }            
+
+            return comments;
+        }
+
+        private Comment ReadComment(SQLiteDataReader reader)
+        {
+            return new Comment
+            {
+                Body = reader.Fetch<string>("Body"),
+                CreatedBy = reader.Fetch<long>("CreatedBy"),
+                CreatedOn = reader.Fetch<DateTime>("CreatedOn"),
+                ID = reader.Fetch<long>("ID"),
+                ModifiedBy = reader.Fetch<long>("ModifiedBy"),
+                ModifiedOn = reader.Fetch<DateTime>("ModifiedOn"),
+                Title = reader.Fetch<string>("Title")
+            };
         }
     }
 }
