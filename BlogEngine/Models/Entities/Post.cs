229e7ec4a76ca96676677ee74e0f8faa20d597b8
Tim Collins <tim@timcollins.ie>
2014-10-06 10:33:32 +0100

    Added better looking cancel button to Create.cshtml. Added missing form element classes to eliminate some ReSharper warnings. Added some attributes to Post entity.

--- BlogEngine/Models/Entities/Post.cs
+++ BlogEngine/Models/Entities/Post.cs
@@ -1,10 +1,18 @@
-﻿namespace BlogEngine.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogEngine.Models.Entities
 {
     public class Post : BaseEntity
     {
         public long ID { get; set; }
+
+        [Display(Name="Category")]
         public long CategoryID { get; set; }
+
+        [Required]
         public string Subject { get; set; }
+
+        [Required]
         public string Body { get; set; }
     }
 }
