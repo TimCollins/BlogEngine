9f3ffce8b36239ff9cd337c9a8ea15b31ed0980c
Tim <tim@timcollins.ie>
2014-10-07 21:25:30 +0100

    Played around with the user display CSS.

--- BlogEngine/Models/Entities/BaseEntity.cs
+++ BlogEngine/Models/Entities/BaseEntity.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace BlogEngine.Models.Entities
 {
     public abstract class BaseEntity
     {
+        [DisplayName("Created On")]
         public DateTime CreatedOn { get; set; }
+        
+        [DisplayName("Modified On")]
         public DateTime ModifiedOn { get; set; }
+
+        [DisplayName("Created By")]
         public long CreatedBy { get; set; }
+        
+        [DisplayName("Modified By")]
         public long ModifiedBy { get; set; }
     }
 }
--- BlogEngine/Models/Util.cs
+++ BlogEngine/Models/Util.cs
@@ -50,6 +50,5 @@ namespace BlogEngine.Models
                 date.Day,
                 date.Year);
         }
-
     }
 }
