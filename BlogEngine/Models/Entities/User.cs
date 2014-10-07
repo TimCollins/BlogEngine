287197a5d70367ef23bf47e7b9fe9dbfef6fb1fd
Tim <tim@timcollins.ie>
2014-10-07 21:12:52 +0100

    Made some changes on how user details are displayed.

--- BlogEngine/Models/Entities/User.cs
+++ BlogEngine/Models/Entities/User.cs
@@ -1,8 +1,12 @@
-﻿namespace BlogEngine.Models.Entities
+﻿using System.ComponentModel;
+
+namespace BlogEngine.Models.Entities
 {
     public class User : BaseEntity
     {
         public int ID { get; set; }
+
+        [DisplayName("Username")]
         public string Name { get; set; }
         public string Password { get; set; }
     }
--- BlogEngine/Models/Repositories/UserRepository.cs
+++ BlogEngine/Models/Repositories/UserRepository.cs
@@ -1,16 +1,43 @@
-﻿using BlogEngine.Models.Entities;
+﻿using System;
+using System.Data.SQLite;
+using BlogEngine.Models.DataAccess;
+using BlogEngine.Models.Entities;
 using BlogEngine.Models.Repositories.Interfaces;
 
 namespace BlogEngine.Models.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        public User GetUserByID(int userID)
+        public User GetUserByID(int id)
         {
-            return new User
+            const string sqlQuery = "SELECT Name, CreatedOn " +
+                                    "FROM User " +
+                                    "WHERE ID = @id";
+
+            User user = new User();
+
+            using (var reader = DbUtil.ExecuteReader(sqlQuery, new SQLiteParameter("@id", id)))
             {
-                Name = "Fred"
+                if (reader.Read())
+                {
+                    user = ReadUser(reader);
+                }
+            }
+
+            user.ID = id;
+
+            return user;
+        }
+
+        private User ReadUser(SQLiteDataReader reader)
+        {
+            User user = new User
+            {
+                Name = reader.Fetch<string>("Name"),
+                CreatedOn = reader.Fetch<DateTime>("CreatedOn")
             };
+            
+            return user;
         }
     }
 }
