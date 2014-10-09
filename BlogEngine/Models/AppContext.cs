1bc1ac7c9a5edc8b9f1747362660220674ef7928
Tim Collins <tim@timcollins.ie>
2014-10-09 15:30:32 +0100

    Looking at biting the bullet and introducing Entity Framework but keeping SQLite. Using this as a starting point: http://hintdesk.com/sqlite-with-entity-framework-code-first-and-migration/

---
+++ BlogEngine/Models/AppContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using BlogEngine.Models.Entities;
+
+namespace BlogEngine.Models
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
